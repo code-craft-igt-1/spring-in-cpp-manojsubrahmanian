68e979b531721c120531579d4be328965b4adf18
Manoj Subrahmanian <manoj.subramanian@gmail.com>
2024-07-18 12:11:40 +0530

    Update stats.cpp to add namespace

--- stats.cpp
+++ stats.cpp
@@ -1,25 +1,27 @@
 #include "stats.h"
 
-// Define the Stats struct
-struct Stats {
-    float average;
-    float min;
-    float max;
-};
-
-Stats Statistics::ComputeStatistics(const std::vector<float>& numbers) {
-    Stats stats;
-    if (numbers.empty()) {
-        stats.average = std::numeric_limits<float>::quiet_NaN();
-        stats.min = std::numeric_limits<float>::quiet_NaN();
-        stats.max = std::numeric_limits<float>::quiet_NaN();
+namespace Statistics {
+    // Define the Stats struct
+    struct Stats {
+        float average;
+        float min;
+        float max;
+    };
+    
+    Stats Statistics::ComputeStatistics(const std::vector<float>& numbers) {
+        Stats stats;
+        if (numbers.empty()) {
+            stats.average = std::numeric_limits<float>::quiet_NaN();
+            stats.min = std::numeric_limits<float>::quiet_NaN();
+            stats.max = std::numeric_limits<float>::quiet_NaN();
+            return stats;
+        }
+    
+        float sum = std::accumulate(numbers.begin(), numbers.end(), 0.0f);
+        stats.average = sum / numbers.size();
+        stats.min = *std::min_element(numbers.begin(), numbers.end());
+        stats.max = *std::max_element(numbers.begin(), numbers.end());
+    
         return stats;
     }
-
-    float sum = std::accumulate(numbers.begin(), numbers.end(), 0.0f);
-    stats.average = sum / numbers.size();
-    stats.min = *std::min_element(numbers.begin(), numbers.end());
-    stats.max = *std::max_element(numbers.begin(), numbers.end());
-
-    return stats;
 }
